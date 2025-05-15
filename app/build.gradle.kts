54c0bbad60ec20eeda2b2edc303afd5db76973dd
leonardomartindev <leonardomartin.dev@gmail.com>
2025-05-15 08:50:48 -0300

    feat: splash screen e tela inicial de seleção de questionários

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.room.common.jvm)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -77,4 +78,7 @@ dependencies {
 
     // LiveData
     implementation(libs.androidx.runtime.livedata)
+
+    implementation(libs.androidx.core.splashscreen)
+
 }
