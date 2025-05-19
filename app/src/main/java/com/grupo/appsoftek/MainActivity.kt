f9e3cc98258afd1ca37bb768c1e562e3ca42e2eb
Thiago Lopes <programadorthiagolopes@gmail.com>
2025-05-19 18:04:07 -0300

    fix: resolvendo problema de importação ambigua

--- app/src/main/java/com/grupo/appsoftek/MainActivity.kt
+++ app/src/main/java/com/grupo/appsoftek/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.grupo.appsoftek
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
